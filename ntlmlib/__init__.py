a895e91c57963c2094ee3b59dafa9ec1863409f6
Ian Clegg <ian.clegg@sourcewarp.com>
2015-03-23 13:25:39 +0000

    added versioneer to perform versioning

--- ntlmlib/__init__.py
+++ ntlmlib/__init__.py
@@ -11,4 +11,36 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+"""
+         _   _           _ _ _
+   _ __ | |_| |_ __ ___ | (_) |__
+  | '_ \| __| | '_ ` _ \| | | '_ \
+  | | | | |_| | | | | | | | | |_) |
+  |_| |_|\__|_|_| |_| |_|_|_|_.__/
+
+  A robust, fast and efficient 'first-class' Python Library for NTLM authentication, signing and encryption
+
+"""
+
+__title__ = 'ntlmlib'
 __author__ = 'ian.clegg@sourcewarp.com'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2015 Ian Clegg'
+
+# Use version information supplied by versioneer during the build
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
+
+
+# Set default logging handler to avoid "No handler found" warnings.
+import logging
+
+try:  # Python 2.7+
+    from logging import NullHandler
+except ImportError:
+    class NullHandler(logging.Handler):
+        def emit(self, record):
+            pass
+
+logging.getLogger(__name__).addHandler(NullHandler())
--- setup.py
+++ setup.py
@@ -13,14 +13,17 @@
 # You should have received a copy of the GNU General Public License
 # along with 'ntlmlib'.  If not, see <http://www.gnu.org/licenses/>.
 import os
-import re
-import sys
-import urllib
+import versioneer
 from setuptools import setup
-from distutils.core import Command
 
-__version__ = '0.0.1'
 project_name = 'ntlmlib'
+
+# versioneer configuration
+versioneer.VCS = 'git'
+versioneer.versionfile_source = os.path.join('ntlmlib', '_version.py')
+versioneer.versionfile_build = os.path.join('ntlmlib', '_version.py')
+versioneer.tag_prefix = ''
+versioneer.parentdir_prefix = 'ntlmlib'
 
 # PyPi supports only reStructuredText, so pandoc should be installed
 # before uploading package
@@ -30,92 +33,9 @@ try:
 except ImportError:
     long_description = ''
 
-# We will run git to get the latest 'tag' and use this to release to PyPi
-import subprocess
-try:
-    process = subprocess.Popen(['git', 'describe', '--tags', '--abbrev=0'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    process.wait()
-    git_tag = process.stdout.readline().strip()
-
-    if process.returncode == 0:
-        # versions must be in the range: 0.0.0 to 999.999.999
-        version_match = re.match('(?:.*)[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(?:.*)', git_tag)
-        if version_match is not None:
-            __version__ = version_match.group()
-        else:
-            print "git tag '%s' is not a valid version number" % git_tag
-    else:
-        print "'git describe' failed to find the latest tag"
-
-except OSError:
-    print "Unable to run 'git describe --abbrev=0', ensure git is correctly installed"
-
-print 'build package version: %s' % __version__
-print 'build package name: %s' % project_name
-
-class BootstrapEnvironmentCommand(Command):
-    description = 'create project development environment from scratch'
-    user_options = []
-
-    def __init__(self, dist):
-        Command.__init__(self, dist)
-        self.project_dir = os.path.abspath(os.path.dirname(__file__))
-        self.temp_dir = os.path.join(self.project_dir, '~temp')
-        self.virtualenv_dir = os.path.join(self.project_dir, 'env')
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
-    def run(self):
-        self.setup_virtual_environment()
-
-    def setup_virtual_environment(self):
-        """Creates Python env if it not exists or re-create if outdated."""
-        if not os.path.exists(self.temp_dir):
-            os.makedirs(self.temp_dir)
-
-        if not os.path.exists(self.virtualenv_dir):
-            virtualenv_setup_path = os.path.join(self.temp_dir, 'virtualenv.py')
-            urllib.urlretrieve(
-                'https://raw.github.com/pypa/virtualenv/master/virtualenv.py',
-                virtualenv_setup_path)
-            virtualenv_prompt = '\({0}\)'.format(project_name) \
-                if os.name == 'posix' else '({0})'.format(project_name)
-            os.system('python {0} --prompt={1} env'.format(
-                virtualenv_setup_path, virtualenv_prompt))
-            self.activate_virtual_environment()
-            easy_install_setup_path = os.path.join(self.temp_dir, 'ez_setup.py')
-            urllib.urlretrieve(
-                'http://peak.telecommunity.com/dist/ez_setup.py',
-                easy_install_setup_path)
-            os.system(
-                'python {0} -U setuptools'.format(easy_install_setup_path))
-            os.system(
-                'pip install -r requirements.txt --download-cache={0}'.format(
-                    self.temp_dir))
-
-    def activate_virtual_environment(self):
-        """ Activates virtual environment in the same way as activate.bat or activate.sh from virtualenv package """
-        if not hasattr(sys, 'real_prefix') or 'VIRTUAL_ENV' not in os.environ:
-            activate_this = '{0}/env/bin/activate_this.py'.format(
-                self.project_dir)
-            virtual_env_path = '{0}/env/bin:'.format(self.project_dir)
-            if os.name != 'posix':
-                activate_this = '{0}/env/Scripts/activate_this.py'.format(
-                    self.project_dir)
-                virtual_env_path = '{0}\env\Scripts;'.format(self.project_dir)
-
-            execfile(activate_this, dict(__file__=activate_this))
-            path = os.environ['PATH']
-            os.environ['PATH'] = virtual_env_path + path
-
-
 setup(
     name=project_name,
-    version=__version__,
+    version=versioneer.get_version(),
     description='A Python library for Windows NTLM authentication, signing and encryption',
     long_description=long_description,
     keywords='ntlm ntlmv2 gss gssapi sign seal authentication'.split(' '),
@@ -126,7 +46,7 @@ setup(
     packages=['ntlmlib'],
     package_data={},
     install_requires=['pycrypto'],
-    cmdclass={'bootstrap_env': BootstrapEnvironmentCommand},
+    cmdclass=versioneer.get_cmdclass(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
